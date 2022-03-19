ef5034e8fed14b54de5245d5230fc48c8f2d2f12
abdulghofur12 <ghofursans12@gmail.com>
2022-03-19 10:58:07 +0700

    menambahkan penjumlahan

--- modul4_1302200018/Program.cs
+++ modul4_1302200018/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace modul4_1302200018
+{
+    public class main
+    {
+        public static void Main(string[]args)
+        {
+            Console.WriteLine(Penjumlahan.JumlahTigaAngka<double>(13, 02, 20));
+            
+        }
+    }
+
+    class  Penjumlahan 
+    {
+        public static T JumlahTigaAngka <T> (T inputsatu, T inputdua, T inputtiga)
+        {
+            dynamic jumlah1 = inputsatu;
+            dynamic jumlah2 = inputdua;
+            dynamic jumlah3 = inputtiga;
+
+            return jumlah1 + jumlah2 + jumlah3;
+
+        }
+    }
+}
