ad61ca8b05f84afc3992a9cb845a955a1d03acd2
abdulghofur12 <ghofursans12@gmail.com>
2022-03-19 12:13:48 +0700

    menambahkan penjumlahan

--- modul4_1302200018/Program.cs
+++ modul4_1302200018/Program.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace modul4_1302200018
 {
-    public class main
+     class Program
     {
-        public static void Main(string[]args)
+         static void Main(string[]args)
         {
             Console.WriteLine(Penjumlahan.JumlahTigaAngka<double>(13, 02, 20));
-            
+            SimpleDataBase<int> data = new SimpleDataBase<int>();
+            data.AddNewData(13);
+            data.AddNewData(02);
+            data.AddNewData(20);
+            data.printAllData();
+
         }
     }
 
@@ -23,5 +31,31 @@ namespace modul4_1302200018
             return jumlah1 + jumlah2 + jumlah3;
 
         }
+
+    }
+    class SimpleDataBase<T>
+    {
+        private List<T> storedData;
+        private List<DateTime> inputDates;
+        public SimpleDataBase()
+        {
+            storedData = new List<T>();
+            inputDates = new List<DateTime>();
+        }
+
+        public void AddNewData(T data)
+        {
+            storedData.Add(data);
+            inputDates.Add(DateTime.Now);
+        }
+
+        public void printAllData()
+        {
+            for (int i = 0; i < storedData.Count; i++)
+            {
+                Console.WriteLine("data" + i + "isi :" + storedData[i].ToString() + "disimpan pada waktu" + i);
+            }
+        }
     }
 }
+   
